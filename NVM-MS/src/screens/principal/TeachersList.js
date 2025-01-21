29296c270ad6edaf88ba4261227f7ab05a530c5d
farine <farine.khan@ary-soft.com>
2025-01-21 16:45:52 +0530

    fixed teachers list ui

--- NVM-MS/src/screens/principal/TeachersList.js
+++ NVM-MS/src/screens/principal/TeachersList.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import CustomSearchBar from '../../customComponents/CustomSearchBar'
 import { COLORS, customStyle } from '../../custom/styles';
@@ -9,15 +9,8 @@ const Tab = createMaterialTopTabNavigator();
 const TeachersList = ({ navigation }) => {
   const [searchActive, setSearchActive] = useState(false);
   const [searchText, setSearchText] = useState(false);
-  const [teachersList, setTeachersList] = useState([
-    {
-      "id": 1,
-      "fullName": "Rajiv Mali",
-      "class": "12",
-      "division": "C",
-      "faculty": "Arts",
-      "phoneNumber": "9876543210"
-    },
+  const [sciTeachersList, setSciTeachersList] = useState([
+   
     {
       "id": 2,
       "fullName": "Amit Sharma",
@@ -27,20 +20,48 @@ const TeachersList = ({ navigation }) => {
       "phoneNumber": "9123456789"
     },
     {
+      "id": 6,
+      "fullName": "Pooja Yadav",
+      "class": "12",
+      "division": "B",
+      "faculty": "Science",
+      "phoneNumber": "9345678901"
+    },
+    {
+      "id": 10,
+      "fullName": "Anjali Reddy",
+      "class": "12",
+      "division": "C",
+      "faculty": "Science",
+      "phoneNumber": "9123987456"
+    },
+    {
+      "id": 12,
+      "fullName": "Sneha Sharma",
+      "class": "12",
+      "division": "B",
+      "faculty": "Science",
+      "phoneNumber": "9823476501"
+    },
+    {
+      "id": 14,
+      "fullName": "Sahil Kapoor",
+      "class": "12",
+      "division": "A",
+      "faculty": "Science",
+      "phoneNumber": "9998765432"
+    },
+  ]
+  );
+  const [comTeachersList, setComTeachersList] = useState([
+   
+    {
       "id": 3,
       "fullName": "Neha Patel",
       "class": "12",
       "division": "B",
       "faculty": "Commerce",
       "phoneNumber": "9988776655"
-    },
-    {
-      "id": 4,
-      "fullName": "Ravi Kumar",
-      "class": "12",
-      "division": "C",
-      "faculty": "Arts",
-      "phoneNumber": "9456123456"
     },
     {
       "id": 5,
@@ -50,13 +71,49 @@ const TeachersList = ({ navigation }) => {
       "faculty": "Commerce",
       "phoneNumber": "9234567890"
     },
+   
     {
-      "id": 6,
-      "fullName": "Pooja Yadav",
+      "id": 9,
+      "fullName": "Shivam Gupta",
       "class": "12",
       "division": "B",
-      "faculty": "Science",
-      "phoneNumber": "9345678901"
+      "faculty": "Commerce",
+      "phoneNumber": "9876123456"
+    },
+    {
+      "id": 13,
+      "fullName": "Vishal Chauhan",
+      "class": "12",
+      "division": "C",
+      "faculty": "Commerce",
+      "phoneNumber": "9456789234"
+    },
+    {
+      "id": 15,
+      "fullName": "Maya Desai",
+      "class": "12",
+      "division": "B",
+      "faculty": "Commerce",
+      "phoneNumber": "9212345678"
+    }
+  ]
+  );
+  const [artsTeachersList, setArtsTeachersList] = useState([
+    {
+      "id": 1,
+      "fullName": "Rajiv Mali",
+      "class": "12",
+      "division": "C",
+      "faculty": "Arts",
+      "phoneNumber": "9876543210"
+    },
+    {
+      "id": 4,
+      "fullName": "Ravi Kumar",
+      "class": "12",
+      "division": "C",
+      "faculty": "Arts",
+      "phoneNumber": "9456123456"
     },
     {
       "id": 7,
@@ -75,22 +132,6 @@ const TeachersList = ({ navigation }) => {
       "phoneNumber": "8765432109"
     },
     {
-      "id": 9,
-      "fullName": "Shivam Gupta",
-      "class": "12",
-      "division": "B",
-      "faculty": "Commerce",
-      "phoneNumber": "9876123456"
-    },
-    {
-      "id": 10,
-      "fullName": "Anjali Reddy",
-      "class": "12",
-      "division": "C",
-      "faculty": "Science",
-      "phoneNumber": "9123987456"
-    },
-    {
       "id": 11,
       "fullName": "Karan Joshi",
       "class": "12",
@@ -98,41 +139,9 @@ const TeachersList = ({ navigation }) => {
       "faculty": "Arts",
       "phoneNumber": "9786123456"
     },
-    {
-      "id": 12,
-      "fullName": "Sneha Sharma",
-      "class": "12",
-      "division": "B",
-      "faculty": "Science",
-      "phoneNumber": "9823476501"
-    },
-    {
-      "id": 13,
-      "fullName": "Vishal Chauhan",
-      "class": "12",
-      "division": "C",
-      "faculty": "Commerce",
-      "phoneNumber": "9456789234"
-    },
-    {
-      "id": 14,
-      "fullName": "Sahil Kapoor",
-      "class": "12",
-      "division": "A",
-      "faculty": "Science",
-      "phoneNumber": "9998765432"
-    },
-    {
-      "id": 15,
-      "fullName": "Maya Desai",
-      "class": "12",
-      "division": "B",
-      "faculty": "Commerce",
-      "phoneNumber": "9212345678"
-    }
+  
   ]
   )
-
   const handlesubmit = () => {
 
   };
@@ -140,15 +149,21 @@ const TeachersList = ({ navigation }) => {
 
   };
 
+
   const renderItems = ({ item }) => {
     return (
-      <View>
-        <CustomTextAndInput type="text"> {item.fullName}</CustomTextAndInput>
-
-      </View>
+      <TouchableOpacity style={styles.itemContainer}>
+        <Image source={{ uri: "https://inventorymanagementdev.s3.us-east-1.amazonaws.com/defaultProfileImage.jpg" }} height={80} width={80} style={styles.image} />
+        <View style={{flex:1}}>
+         <CustomTextAndInput type="text" style={{fontSize:18}}>{item.fullName}</CustomTextAndInput>
+          <View>
+            <CustomTextAndInput type="text">Class: {item.class} {item.division}</CustomTextAndInput>
+            <CustomTextAndInput type="text">Phone: {item.phoneNumber}</CustomTextAndInput>
+          </View>
+        </View>
+      </TouchableOpacity>
     );
   }
-
 
   return (
     <SafeAreaView style={customStyle.container}>
@@ -182,48 +197,65 @@ const TeachersList = ({ navigation }) => {
               <CustomTextAndInput type="text" style={[styles.label, { color: focused ? "#ffffff" : "#292929" }]}>{route.name}</CustomTextAndInput>
             </View>
           ),
-        })}
-        sceneContainerStyle={{
-          backgroundColor: "red",
-          paddingTop: 100,
-        }}>
+        })}>
+
         <Tab.Screen name="Science">
           {() => (
-            <View>
-              <CustomTextAndInput type="text"> Science</CustomTextAndInput>
+        
               <FlatList
-              data={teachersList}
-              renderItem={renderItems}
+                data={sciTeachersList}
+                renderItem={renderItems}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={styles.flatlistStyle}
               />
-            </View>
+
 
           )}
         </Tab.Screen>
         <Tab.Screen name="Commerce">
           {() => (
-            <View>
-              <CustomTextAndInput type="text"> Commerce</CustomTextAndInput>
               <FlatList
-              data={teachersList}
-              renderItem={renderItems}
-              />
-            </View>)}
+                data={comTeachersList}
+                renderItem={renderItems}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={styles.flatlistStyle}
+              />)}
         </Tab.Screen>
         <Tab.Screen name="Arts">
           {() => (
-            <View>
-              <CustomTextAndInput type="text"> arts</CustomTextAndInput>
+    
               <FlatList
-              data={teachersList}
-              renderItem={renderItems}
-              />
-            </View>)}
+                data={artsTeachersList}
+                renderItem={renderItems}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={styles.flatlistStyle}
+              />)}
         </Tab.Screen>
       </Tab.Navigator>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
-
+  itemContainer: {
+    flexDirection: "row",
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor:"#B8BEC6",
+    gap:20,
+    backgroundColor:"#FAFBFD",
+    alignItems:"center",
+    borderRadius:10,
+  },
+  flatlistStyle: {
+    gap: 10,
+    padding: 12,
+    backgroundColor:"#FFFFFF",
+    paddingTop:20,
+    flex:1,
+  },
+  image:{
+    borderRadius:50
+  }
 });
+
 export default TeachersList;
